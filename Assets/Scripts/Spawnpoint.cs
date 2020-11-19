4b594a714269a57e610b50594a4cf574eff685c3
hege22 <72064517+hege22@users.noreply.github.com>
2020-11-19 11:56:24 +0200

    Add files via upload

---
+++ Assets/Scripts/Spawnpoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnpoint : MonoBehaviour
+{
+
+    public GameObject Obstacle;
+
+    void Start()
+    {
+        Instantiate(Obstacle, transform.position, Quaternion.identity);
+    }
+}
